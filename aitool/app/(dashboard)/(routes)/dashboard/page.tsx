06a6bd381725e7416c066015abf00475d7b7efa1
JoniMop <jonatangurari@gmail.com>
2023-09-25 10:21:16 -0600

    Fix hyper error - Adding Card - Highligh selected by default

--- aitool/app/(dashboard)/(routes)/dashboard/page.tsx
+++ aitool/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,41 @@
 //import { UserButton } from "@clerk/nextjs";
 
+"use client"
+import {Card} from "@/components/ui/card";
+
+import {MessageSquare} from "lucide-react";
+
+const tools = [
+ {
+  label: "Conversation",
+  icon: MessageSquare,
+  color: "text-violet-500",
+  bgColor: "bg-violet-500/10",
+  href: "/conversation"
+ }
+]
 const DashboardPage = () => {
     return (
         <div>
-            <p>Dashboard Page (Protected)</p>
+	  <div className="mb-8 space-y-4 ">
+	    <h2 className="text-2x1 md:text-4x1 font-bold text-center">
+	     Explore de Power of AI
+	    </h2>
+	    <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+	      Chat with AI - Experience its power
+	    </p>
+	  </div>
+	  <div className="px-4 md:px-20 lg:px-32 space-y-4">
+             {tools.map((tool) => (
+	       <Card 
+	         key={tool.href}
+		 className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+	       >
+
+	       </Card> 
+	     ))}	       
+	  </div>
+           {/* <p>Dashboard Page (Protected)</p> */}
            {/*  <UserButton afterSignOutUrl="/" /> this was to show gmail profile and sign out from there  */}
         </div>
     )
--- aitool/components/mobile-sidebar.tsx
+++ aitool/components/mobile-sidebar.tsx
@@ -4,8 +4,19 @@ import { Menu } from "lucide-react";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
 import { Button } from "@/components/ui/button";
+import {useEffect, useState} from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false); //fixing Hydration errors
+
+  useEffect(() => {
+    setIsMounted(true);
+    }, []);
+
+  if (!isMounted) {
+    return null;
+    }
+
  return (
    <Sheet>
     <SheetTrigger>
